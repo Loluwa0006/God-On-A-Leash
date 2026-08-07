23229b7d018a77fd37565963425fb8ba4416848b
Loluwa0006 <temiloluwa.awosiyan@ontariotechu.net>
2026-08-07 11:49:08 -0400

    Speedline VFX Fix
    Fixed issues where
    
    - speed lines wouldn't cover the screen
    - speed lines became weird while swinging
    - speed lines were too visually noisy

--- Assets/Scripts/Entities/Player/VFX/SpeedlinesManager.cs
+++ Assets/Scripts/Entities/Player/VFX/SpeedlinesManager.cs
@@ -10,6 +10,8 @@ public class SpeedlinesManager : MonoBehaviour
 
     [SerializeField] float maxSpeedToConsider = 350.0f;
     [SerializeField] float minSpeedToConsider = 100.0f;
+    [SerializeField] float minEmissionRate = 25.0f;
+    [SerializeField] float maxEmissionRate = 50.0f;
     public void FixedUpdate()
     {
         Vector2 lateralSpeed = new (player.RigidBody.linearVelocity.x, player.RigidBody.linearVelocity.z);
@@ -21,8 +23,9 @@ public class SpeedlinesManager : MonoBehaviour
         else
         {
             var speedAsPercentage = Mathf.Clamp01((lateralSpeed.magnitude - minSpeedToConsider) / (maxSpeedToConsider - minSpeedToConsider));
-            var emissionRate = speedToEmissionRate.Evaluate(speedAsPercentage);
+            var emissionRate = Mathf.Lerp(minEmissionRate, maxEmissionRate, speedToEmissionRate.Evaluate(speedAsPercentage));
             emission.rateOverTime = emissionRate;
+            speedlineParticles.transform.rotation = Quaternion.LookRotation(-player.RigidBody.linearVelocity.normalized, Vector3.up);
         }
     }
 }
