d34f7c98e8b6557212f0dccd40ff3f4d211df69f
Loluwa0006 <temiloluwa.awosiyan@ontariotechu.net>
2026-08-09 08:04:15 -0400

    Parry Size Scaling with Rod Length Increase
    Massively increased parry size scaling with rod length (4 -> 85)
    
    Re implemented requiring retracted rod for swinging

--- Assets/Scripts/Entities/Player/States/PlayerSwingState.cs
+++ Assets/Scripts/Entities/Player/States/PlayerSwingState.cs
@@ -95,7 +95,9 @@ public class PlayerSwingState : PlayerAirState
 
     public override bool StateAvailable()
     {
-        if (GrappleUtilities.AimingAtGrappable(Player, Player.RodManager.GrappleMask) && Player.PlayerInput.BufferRegistry[InputManager.BufferableInputs.Swing].Buffered)
+        if (GrappleUtilities.AimingAtGrappable(Player, Player.RodManager.GrappleMask)
+            && Player.PlayerInput.BufferRegistry[InputManager.BufferableInputs.Swing].Buffered && 
+            Player.RodManager.RodLength <= 0.01f)
         {
             return true;
         }
